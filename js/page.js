91c79be3d4e5ea7eff7aeffb6449effdcebd8111
Casey Thomas <cpthomas@gmail.com>
2012-09-01 19:54:28 -0400

    Disable fixed header update when page is too short for scroll bar

--- js/page.js
+++ js/page.js
@@ -7,24 +7,27 @@ $(document).ready(function(){
 
     $(window).scroll(function(){
         var toTop = $(window).scrollTop();
+        
+        if ($("body").height() > $(window).height()) {
 
-        //scrolling down, and not snaping back
-        if (prevToTop < toTop && toTop >= 0) {
-            if (toTop <= navToTop) {
-                $nav.css('top', navToTop - toTop);
-            } else if (toTop > navToTop) {
-                $nav.css('top', 0);
-                $title.show();
-                $nav.addClass("scroll")
+            //scrolling down, and not snaping back
+            if (prevToTop < toTop && toTop >= 0) {
+                if (toTop <= navToTop) {
+                    $nav.css('top', navToTop - toTop);
+                } else if (toTop > navToTop) {
+                    $nav.css('top', 0);
+                    $title.show();
+                    $nav.addClass("scroll")
+                }
+            //scrolling up
+            } else if (prevToTop > toTop) {
+                if (toTop < navToTop && toTop >= 0)  {
+                    $nav.css('top', navToTop - toTop);
+                    $title.hide();
+                    $nav.removeClass("scroll")
+                }
             }
-        //scrolling up
-        } else if (prevToTop > toTop) {
-            if (toTop < navToTop && toTop >= 0)  {
-                $nav.css('top', navToTop - toTop);
-                $title.hide();
-                $nav.removeClass("scroll")
-            }
-        } 
+        }
 
         prevToTop = toTop;
     });
